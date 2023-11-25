bdd5fae71e37d3bd12992c5dc5a004b960ebcc2c
donald.osgood@phrehab.com <donald.osgood@phrehab.com>
2023-11-24 22:54:54 -0600

    add modular structure with bootstrap and blueprints

---
+++ eco_system/main.py
@@ -0,0 +1,20 @@
+""" 
+@Program: app
+@Author: Donald Osgood
+@Last Date: 2023-11-22 22:55:33
+@Purpose:Donald Osgood
+"""
+# Flask application code
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from generators.view import view_generator_blueprint
+from generators.apis import apis_generator_blueprint
+
+app = Flask(__name__)
+Bootstrap(app)
+app.register_blueprint(view_generator_blueprint)
+app.register_blueprint(apis_generator_blueprint)
+
+if __name__ == "__main__":
+    app.run(debug=True)
